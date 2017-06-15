262b9d548697b3664b63f40fe8dd8f7d9d650300
Robert Gleeson <robert.gleeson@softwaredesign.ie>
2017-06-15 16:48:23 +0100

    support for git lab

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const ini = require('ini');
 const colors = require('colors/safe');
-const spawn = require('child_process').spawn
+const open = require('open');
 
 try {
     const config = ini.parse(fs.readFileSync('.git/config', 'utf-8'));
@@ -10,7 +10,16 @@ try {
         console.log(colors.red('No Git repo found'));
         return;
     }
-    spawn('open', [config['remote "origin"'].url]);
+
+    var url = config['remote "origin"'].url;
+    if (url.substring(0, 3) == "git") {
+        url = url.substring(4);
+        url = url.substring(0, url.length - 4);
+        url = url.replace(':', '/');
+        open(`http://${url}`);
+    } else {
+        open(url);
+    }
 } catch (e) {
     console.log(colors.red('No repo found!'));
 }
