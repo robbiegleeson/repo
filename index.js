a34804e3b5e33eb640776bbb8453ab7fc8c832a9
Graham Whelan <gawhelan@gmail.com>
2017-06-16 21:33:13 +0100

    Open the GitHub/GitLab page for current branch/tag

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const fs = require('fs');
 const ini = require('ini');
 const colors = require('colors/safe');
 const open = require('open');
+const child_process = require('child_process');
+
+const GET_CURRENT_BRANCH_CMD = 'git symbolic-ref -q --short HEAD';
+const GET_CURRENT_TAG_CMD = 'git describe --tags --exact-match';
+
+function execSync(cmd) {
+    try {
+        return child_process.execSync(cmd).toString().trim();
+    } catch (err) {
+        return '';
+    }
+}
+
+function getCurrentBranch() {
+    return execSync(GET_CURRENT_BRANCH_CMD) || execSync(GET_CURRENT_TAG_CMD);
+}
 
 try {
     const config = ini.parse(fs.readFileSync('.git/config', 'utf-8'));
@@ -12,13 +28,15 @@ try {
     }
 
     var url = config['remote "origin"'].url;
+    const branch = getCurrentBranch();
+
     if (url.substring(0, 3) == "git") {
         url = url.substring(4);
         url = url.substring(0, url.length - 4);
         url = url.replace(':', '/');
-        open(`http://${url}`);
+        open(`http://${url}/tree/${branch}`);
     } else {
-        open(url);
+        open(`${url}/tree/${branch}`);
     }
 } catch (e) {
     console.log(colors.red('No repo found!'));
