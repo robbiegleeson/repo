5128d71008ee8036c8997ba8e59a1753b5ce3d8c
Rob Gleeson <robbie.gleeson@gmail.com>
2017-06-20 07:25:26 +0100

    Fix issue where opening 404 page when origin doesn't exist

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const ini = require('ini');
 const colors = require('colors/safe');
 const open = require('open');
 const child_process = require('child_process');
+const request = require('request');
 
 const urls = require('./urls');
 
@@ -11,32 +12,35 @@ const GET_CURRENT_BRANCH_CMD = 'git symbolic-ref -q --short HEAD';
 const GET_CURRENT_TAG_CMD = 'git describe --tags --exact-match';
 
 function execSync(cmd) {
-    try {
-        return child_process.execSync(cmd).toString().trim();
-    } catch (err) {
-        return '';
-    }
+  try {
+    return child_process.execSync(cmd).toString().trim();
+  } catch (err) {
+    return '';
+  }
 }
 
 function getCurrentBranch() {
-    return execSync(GET_CURRENT_BRANCH_CMD) || execSync(GET_CURRENT_TAG_CMD);
+  return execSync(GET_CURRENT_BRANCH_CMD) || execSync(GET_CURRENT_TAG_CMD);
 }
 
-try {
-    const config = ini.parse(fs.readFileSync('.git/config', 'utf-8'));
-    if (!config) {
-        console.log(colors.red('No Git repo found'));
-        return;
-    }
+function openRepo() {
+  const config = ini.parse(fs.readFileSync('.git/config', 'utf-8'));
+  if (!config) {
+    console.log(colors.red('No Git repo found'));
+    return;
+  }
 
-    const url = urls.getHttpsUrlFromRemote(config['remote "origin"'].url);
-    const branch = getCurrentBranch();
+  const url = urls.getHttpsUrlFromRemote(config['remote "origin"'].url);
+  const branch = getCurrentBranch();
 
-    if (branch) {
+  request.get(`${url}/tree/${branch}`)
+    .on('response', function(response) {
+      if (response.statusCode !== 404) {
         open(`${url}/tree/${branch}`);
-    } else {
+      } else {
         open(url);
-    }
-} catch (e) {
-    console.log(colors.red('No repo found!'));
+      }
+    });
 }
+
+openRepo();
